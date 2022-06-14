6cd296f8247f166867bba716e772ef1821db639e
Erik Böck <89930804+erikbocks@users.noreply.github.com>
2022-06-14 20:30:30 -0300

    caminho dos audios

--- src/services/audio.js
+++ src/services/audio.js
@@ -1,8 +1,8 @@
 let audiosArray =
   [
-    { "name": "rapaz", "sound": "src\\audio\\xaropinho.mp3" },
-    { "name": "tome", "sound": "src\\audio\\tome.mp3" },
-    { "name": "filho", "sound": "src\\audio\\filho.mp3" }
+    { "name": "rapaz", "sound": "src/audio/xaropinho.mp3" },
+    { "name": "tome", "sound": "src/audio/tome.mp3" },
+    { "name": "filho", "sound": "src/audio/filho.mp3" }
   ]
 
 module.exports = {
