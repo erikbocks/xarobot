47476ee5c1a5a095a8045bd5616cec973b1c7242
erikbocks <erik.bock@outlook.com>
2022-06-27 23:56:15 -0300

    atualizei o comando de ajuda

--- index.js
+++ index.js
@@ -137,7 +137,7 @@ client.on("messageCreate", message => {
     const ajuda = new Discord.MessageEmbed()
       .setColor("YELLOW")
       .setTitle("**Ajuda do Xarobot**")
-      .setDescription('Oi, eu sou o Xarobot! Fui feito apenas para ser algo engraçado, sendo assim, eu conto com alguns comandos básicos.\n\n !fale rapaz : entra na call, reproduz o famoso audio "rapazz" e desconecta\n !fale tome : entra na call, reproduz o áudio "tome" e desconecta \n !fale filho : entra na call, reproduz o áudio "que isso meu filho, calma" e desconecta')
+      .setDescription('Oi, eu sou o Xarobot! Fui feito apenas para ser algo engraçado, sendo assim, eu conto com alguns comandos básicos.\n\n !fale rapaz : entra na call, reproduz o famoso audio "rapazz" e desconecta\n !fale tome : entra na call, reproduz o áudio "tome" e desconecta \n !fale filho : entra na call, reproduz o áudio "que isso meu filho, calma" e desconecta \n !fale gosta : entra na call, reproduiz o audio "ele gostaa" e desconecta \n !fale ui : entra na call, fala o "uiii" e desconecta')
 
     message.reply({ embeds: [ajuda] })
   }
